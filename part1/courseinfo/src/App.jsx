b37ebc23ca80039d7de45811f02c7e264d9ba729
mahrukh98 <tariq.mahrukh98@gmail.com>
2024-09-19 23:32:22 +0500

    destructure props for readability

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,32 +1,31 @@
-const Header = (props) => {
+const Header = ({course}) => {
   return (
     <div>
-      <h1>{props.course}</h1> 
+      <h1>{course}</h1> 
     </div>
   )
 }
 
-
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <div>
       <p>
-        {props.parts[0].name} {props.parts[0].exercises}
+        {parts[0].name} {parts[0].exercises}
       </p>
       <p>
-        {props.parts[1].name} {props.parts[1].exercises}
+        {parts[1].name} {parts[1].exercises}
       </p>
       <p>
-        {props.parts[2].name} {props.parts[2].exercises}
+        {parts[2].name} {parts[2].exercises}
       </p>
     </div>
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
   return (
     <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     </div>
   )
 }
