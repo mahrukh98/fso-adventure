a9a42c63ebd0b2845b61f862e7d1990ef029f154
mahrukh98 <tariq.mahrukh98@gmail.com>
2024-09-20 13:31:49 +0500

    add exercise 1.5 solution

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,18 +34,21 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  // an array consisting of parts as objects
-  const parts = [
-    {name: 'Fundamentals of React' , exercises: 10},
-    {name: 'Using props to pass data' , exercises: 7},
-    {name: 'State of a component', exercises: 14}
-  ]
-  return (
+  // a single object for both course name and the parts such that parts is an array consisting 
+  // of part objects 
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React' , exercises: 10},
+      {name: 'Using props to pass data' , exercises: 7},
+      {name: 'State of a component', exercises: 14}
+    ]
+  } 
+    return (
     <div>
-      <Header course={course} />
-      <Content parts = {parts} />
-      <Total parts = {parts}/>
+      <Header course={course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts}/>
     </div>
   )
 }
